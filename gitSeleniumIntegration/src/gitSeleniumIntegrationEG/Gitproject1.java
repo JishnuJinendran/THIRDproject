b6f1a469546ecffed53391e532d4ee9e4fdb73fe
JISHNU <jishnujg2003@gmail.com>
2026-01-08 10:07:38 +0530

    first commit from eclipse

---
+++ gitSeleniumIntegration/src/gitSeleniumIntegrationEG/Gitproject1.java
@@ -0,0 +1,11 @@
+package gitSeleniumIntegrationEG;
+
+public class Gitproject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello stc ");
+	}
+
+}
+
